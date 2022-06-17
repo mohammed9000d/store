1c90170858a341b9a9473707bddded3bf40e265f
mohammed9000d <mohammed9000dd@gmail.com>
2022-06-17 17:34:46 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 })->name('admin.dashboard');
 
 Route::get('/dashboard', function () {
